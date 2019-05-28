980e8cdd75d9a42fdd8c62cbc004a5fe3629abad
Linkslumps <evanontario@hotmail.com>
2019-05-28 14:59:38 -0300

    vue and vuepress installed before hello world

---
+++ themes/ketikidis-octobercms-success/assets/js/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import appPortfolio from './components/appPortfolio.vue'
+
+new Vue({
+    el: '#appPortfolio',
+    render: h => h(appPortfolio)
+})
---
+++ themes/ketikidis-octobercms-success/assets/js/components/appPortfolio.vue
@@ -0,0 +1,17 @@
+<script>
+
+    Vue.component('portfolio', {
+        template: '#image-render',
+        data: function () {
+            return {
+                hello: 'Hello'
+            }
+        }
+    })
+
+    export default {
+        components: {
+            appPortfolio: portfolio
+        }
+    }
+</script>
